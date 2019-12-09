021c5e94a3b75ecddb4321e3a60f251b48b916b2
李福荣 <774238577@qq.com>
2019-12-09 19:58:34 +0800

    更换为阿里云服务器

--- QDPJCustomerInfo/Constants.cs
+++ QDPJCustomerInfo/Constants.cs
@@ -11,10 +11,12 @@ namespace QDPJCustomerInfo
         // URL of REST service
         //public static string RestUrl = "https://developer.xamarin.com:8081/api/todoitems/{0}";
         //public static string RestUrl = "http://62.234.107.212:8001/api/CustomerInfos/{0}";
-        public static string RestUrl = "http://62.234.107.212:8001/api/CustomerInfos/{0}";
-        public static string AddressesUrl = "http://62.234.107.212:8001/api/Addresses/{0}";
+        //public static string RestUrl = "http://62.234.107.212:8001/api/CustomerInfos/{0}";
+        //public static string AddressesUrl = "http://62.234.107.212:8001/api/Addresses/{0}";
+        public static string RestUrl = "http://123.57.5.116:8001/api/CustomerInfos/{0}";
+        public static string AddressesUrl = "http://123.57.5.116:8001/api/Addresses/{0}";
         // Credentials that are hard coded into the REST service
-        public static string Username = "ubuntu";
+        public static string Username = "root";
         public static string Password = "Hua5211314";
     }
 }
